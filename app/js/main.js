05fc282b76f9df79859957b4d898a21683e656dd
Andrew Basiuk <a.basyuk4779@gmail.com>
2018-06-13 20:17:16 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -10,41 +10,78 @@ $(document).ready(function() {
     });
     //end menu show and hide
 
-
     var currentYear = new Date().getFullYear();
     document.querySelector('.age').innerHTML = currentYear - 1994;
 
 
 
 
+    // function animationScroll(obj) {
+    //     for(var key in obj) { 
+    //         var elements = document.querySelectorAll("." + key);
+    //         var arr = [];
+
+    //         for(var i = 0; i < elements.length; i++) {
+    //             arr.push(elements[i]);
+    //         }
+
+    //         var arrTop = arr.map(function(item) {
+    //            return item.getBoundingClientRect().top;
+    //         });
+
+    //         window.onscroll = function() {
+    //             var windowScroll = window.pageYOffset;
+
+    //             for(var j = 0; j < arr.length; j++ ) {
+    //                 var difference = arrTop[j] - windowScroll;
+
+    //                 if(difference < 500) {
+    //                     arr[j].classList.add(obj[key]);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 
 
+    function animationOnScroll(el) { 
+            var elements = document.querySelectorAll(el);
+            var arrElement = [];
 
+            for(var i = 0; i < elements.length; i++) {
+                arrElement.push(elements[i]);
+            }
 
+            var arrTop = arrElement.map(function(item) {
+               return item.getBoundingClientRect().top;
+            });
 
+            window.onscroll = function() {
+                var windowScroll = window.pageYOffset;
 
+                for(var j = 0; j < arrElement.length; j++ ) {
+                    var difference = arrTop[j] - windowScroll;
 
+                    if(difference < 500) {
+                        if(arrElement[j].classList.contains("fade")) {
+                            arrElement[j].classList.add("fade-in");
+                        } else if(arrElement[j].classList.contains("slide-right-down")) {
+                            arrElement[j].classList.add("slide-right-down-in");
+                        } else if(arrElement[j].classList.contains("slide-right")) {
+                            arrElement[j].classList.add("slide-right-in");
+                        } else if(arrElement[j].classList.contains("slide-left")) {
+                            arrElement[j].classList.add("slide-left-in");
+                        } else if(arrElement[j].classList.contains("slide-top")) {
+                            arrElement[j].classList.add("slide-top-in");
+                        } else if(arrElement[j].classList.contains("slide-bottom")) {
+                            arrElement[j].classList.add("slide-bottom-in");
+                        } 
+                    }
+                }
+            }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    animationOnScroll(".animation"); 
 
 
 
