4afecf559f83f2e1e6edf639d334e19859454c1e
Andrii Basiuk <andrii.basiuk@mgid.com>
2021-02-19 09:59:25 +0200

    add pass validation and validations text

--- portfolio/src/app/chat/components/login/login.component.ts
+++ portfolio/src/app/chat/components/login/login.component.ts
@@ -1,8 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {RegistrationTypes} from '../../chat.component';
 import {CustomValidationService} from '../../../services/custom-validation.service';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -26,12 +27,37 @@ export class LoginComponent implements OnInit {
   createForm() {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, this.customValidationService.emailValid]],
-      pass: [''],
+      pass: ['', [Validators.required, this.customValidationService.passwordValid]],
     });
+  }
+
+  getField(fieldName: string): AbstractControl {
+    return this.loginForm.get(fieldName);
+  }
+
+  errorMessage(fieldName: string): string {
+    let errorText = '';
+
+    switch (fieldName) {
+      case 'email':
+        errorText = 'wrong email';
+        break;
+
+      case 'pass':
+        errorText = 'minimum eight characters, at least one letter and one number:';
+        break;
+
+      default:
+        errorText = '';
+    }
+
+    return errorText;
   }
 
   onSubmit() {
     this.type === this.registrationTypes.SignIn ? this.login() : this.signIn();
+
+    console.log(this.loginForm.get('email').dirty, this.loginForm.get('email').invalid);
   }
 
   login() {
--- portfolio/src/app/services/custom-validation.service.ts
+++ portfolio/src/app/services/custom-validation.service.ts
@@ -7,14 +7,26 @@ import {FormControl} from '@angular/forms';
 export class CustomValidationService {
 
   emailValid(control: FormControl) {
-    const re = /^([a-zA-Z0-9_\+\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(
-      control.value
-    );
+    const re = /^([a-zA-Z0-9_\+\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(control.value);
+
     if (re) {
       return null;
     }
+
     return {
       invalidEmail: true,
+    };
+  }
+
+  passwordValid(control: FormControl) {
+    const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(control.value);
+
+    if (re) {
+      return null;
+    }
+
+    return {
+      invalidPass: true,
     };
   }
 
